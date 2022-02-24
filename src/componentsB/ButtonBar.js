784ac200820d4586d945edd9293dd1a698758011
aavisrur-makor <aavisrur@makor-capital.com>
2022-02-24 13:01:59 +0200

    fixed product picker

--- src/componentsB/ButtonBar.js
+++ src/componentsB/ButtonBar.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useStyles } from '../styles/mainStyles';
+import React, { useState, useEffect, useRef } from "react";
+import { useStyles } from "../styles/mainStyles";
 import {
   Grid,
   Box,
@@ -18,31 +18,39 @@ import {
   Collapse,
   Toolbar,
   AppBar,
-} from '@material-ui/core';
-import { openWebSocket } from '../styles/utils/utilsFunction';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import { IOSSwitch } from '../styles/utils/IosRadio';
-import * as webSocketService from '../services/websocket';
-import { PinDropSharp } from '@material-ui/icons';
-import MultipleSelect from './MultipleSelect';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import { currentWorker } from '../App';
-import Switch from '@material-ui/core/Switch';
-import { PurpleSwitch } from '../styles/GreenSwitch';
+} from "@material-ui/core";
+import { openWebSocket } from "../styles/utils/utilsFunction";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import { IOSSwitch } from "../styles/utils/IosRadio";
+import * as webSocketService from "../services/websocket";
+import { PinDropSharp } from "@material-ui/icons";
+import MultipleSelect from "./MultipleSelect";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import { currentWorker } from "../App";
+import Switch from "@material-ui/core/Switch";
+import { PurpleSwitch } from "../styles/GreenSwitch";
 
-let type = 'get_data';
+let type = "get_data";
 
 const ButtonBar = (props) => {
   const changeMode = props.changeMode;
   const [showFilters, setShowFilters] = useState(false);
   const [filtersChanged, setFiltersChanged] = useState(false);
-  const [setGroupByType, setGroupBySide, setGroupByLocation, setGroupByThread] = props.groupBySetters;
+  const [setGroupByType, setGroupBySide, setGroupByLocation, setGroupByThread] =
+    props.groupBySetters;
   const filters = props.filters;
   const setFilters = props.setFilters;
-  console.log('props', props);
+  console.log("props", props);
   const [numberOfThreads, setNumOfThreads] = useState(0);
   const [power, setPower] = useState(false);
-  const [setStateTrades, setStateTradesPartly, setTypeTrades, setSideTrades, setLocationTrades, setThreadTrades] = props.dataSetters;
+  const [
+    setStateTrades,
+    setStateTradesPartly,
+    setTypeTrades,
+    setSideTrades,
+    setLocationTrades,
+    setThreadTrades,
+  ] = props.dataSetters;
   const classes = useStyles();
   useEffect(() => {}, [power]);
   const handleShowFilters = () => {
@@ -71,7 +79,7 @@ const ButtonBar = (props) => {
     }
 
     data = {
-      mode: props.mode ? 'stress' : 'regular',
+      mode: props.mode ? "stress" : "regular",
       type,
 
       power: !power,
@@ -95,7 +103,12 @@ const ButtonBar = (props) => {
     setPower((prev) => !prev);
   };
   return (
-    <AppBar style={{ backgroundColor: 'transparent' , borderRadius: '0px 0px 30px 30px'}}>
+    <AppBar
+      style={{
+        backgroundColor: "transparent",
+        borderRadius: "0px 0px 30px 30px",
+      }}
+    >
       <Toolbar>
         <Grid container alignItems="center" justifyContent="space-between">
           <Grid item xs={5}>
@@ -104,7 +117,13 @@ const ButtonBar = (props) => {
                 <Typography>Server test graph</Typography>
               </Grid>
               <Grid item xs={3}>
-                <MultipleSelect disabled={power} options={['a server', 'dfgfg']} label="servers" setFilters={setFilters} values={filters.servers} />
+                <MultipleSelect
+                  disabled={power}
+                  options={["a server", "dfgfg"]}
+                  label="servers"
+                  setFilters={setFilters}
+                  values={filters.servers}
+                />
               </Grid>
               <Grid item xs={2} style={{ marginLeft: 15 }}>
                 <FormControlLabel
@@ -122,9 +141,15 @@ const ButtonBar = (props) => {
               </Grid>
               {props.mode && (
                 <Grid item xs={3}>
-                  <FormControl className={classes.TextFieldInput} variant="outlined" size="small" fullWidth disabled={power}>
+                  <FormControl
+                    className={classes.TextFieldInput}
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                    disabled={power}
+                  >
                     <TextField
-                      style={{ color: '#848E9C' }}
+                      style={{ color: "#848E9C" }}
                       className={classes.input}
                       onChange={(e) => {
                         setNumOfThreads(e.target.value);
@@ -140,27 +165,50 @@ const ButtonBar = (props) => {
             </Grid>
           </Grid>
           <Grid item xs={5}>
-            <Grid container justifyContent="flex-end" alignItems="center" spacing={2}>
+            <Grid
+              container
+              justifyContent="flex-end"
+              alignItems="center"
+              spacing={2}
+            >
               <Grid item xs={3}>
-                <MultipleSelect disabled={power} options={['FOK', 'MKT', 'RFQ']} label="types" setFilters={setFilters} values={filters.types} />
+                <MultipleSelect
+                  disabled={power}
+                  options={["FOK", "MKT", "RFQ"]}
+                  label="types"
+                  setFilters={setFilters}
+                  values={filters.types}
+                />
               </Grid>
               <Grid item xs={3}>
-                <MultipleSelect disabled={power} options={['Buy', 'Sell']} label="sides" setFilters={setFilters} values={filters.sides} />
+                <MultipleSelect
+                  disabled={power}
+                  options={["Buy", "Sell"]}
+                  label="sides"
+                  setFilters={setFilters}
+                  values={filters.sides}
+                />
               </Grid>
               <Grid item xs={3}>
-                <MultipleSelect disabled={power} options={props.products} label="products" setFilters={setFilters} values={filters.products} isObjectOptions />
+                <MultipleSelect
+                  disabled={power}
+                  options={props.products}
+                  label="products"
+                  setFilters={setFilters}
+                  values={filters.products}
+                />
               </Grid>
               <Grid item xs={2} direction="row">
                 <Button
                   fullWidth
                   onClick={createTrade}
                   style={{
-                    backgroundColor: 'lightblue',
+                    backgroundColor: "lightblue",
                     flex: 1,
-                    textTransform: 'none',
+                    textTransform: "none",
                   }}
                 >
-                  {!power ? 'Start' : 'Stop'}
+                  {!power ? "Start" : "Stop"}
                 </Button>
               </Grid>
             </Grid>
